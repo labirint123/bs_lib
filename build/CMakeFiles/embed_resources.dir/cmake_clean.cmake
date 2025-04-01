ab44586fb39671e4cb3f707d6cdb022b7e40ce0c
labirint123 <labirintx2@gmail.com>
2025-04-01 22:38:28 +0700

    removed

--- build/CMakeFiles/embed_resources.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/embed_resources"
-  "Raleway.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/embed_resources.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
